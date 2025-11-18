6bd765af4478f84f27045fd92f87c27088d60f83
sfdfsd3223 <H098S02828@chunchon.gwe.hs.kr>
2025-11-18 13:49:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,10 +71,16 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     
      labels[0]: {
        "texts": ["짬뽕의 특징과 유래", "국물 맛 포인트", "지역별 스타일 차이"],
-       "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
+       "images": [https://www.mexican.co.kr/main/img/sub/chicken02.png"],
        "videos": ["https://youtu.be/XXXXXXXXXXX"]
      },
 
+
+     labels[1]: {
+       "texts": ["파스타의 특징과 유래", "국물 맛 포인트", "지역별 스타일 차이"],
+       "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
+       "videos": ["https://youtu.be/XXXXXXXXXXX"]
+     },    
 
      labels[1]: {
        "texts": ["파스타의 특징과 유래", "국물 맛 포인트", "지역별 스타일 차이"],
