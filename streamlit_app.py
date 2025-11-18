3984c571c9cb39f0918abe0381bb611e87e3257f
sfdfsd3223 <H098S02828@chunchon.gwe.hs.kr>
2025-11-18 13:49:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     
      labels[0]: {
        "texts": ["짬뽕의 특징과 유래", "국물 맛 포인트", "지역별 스타일 차이"],
-       "images": [https://www.mexican.co.kr/main/img/sub/chicken02.png"],
+       "images": ["https://www.mexican.co.kr/main/img/sub/chicken02.png"],
        "videos": ["https://youtu.be/XXXXXXXXXXX"]
      },
 
